1a30b9dcce81cad7ff1d30cf600e40f2862ef1c7
HuanMeng-official <3099044113@qq.com>
2024-02-04 20:16:45 +0800

    添加ChantFormat开关

--- src/main/java/me/huanmeng/player/ChatFormat.java
+++ src/main/java/me/huanmeng/player/ChatFormat.java
@@ -1,24 +1,28 @@
 package me.huanmeng.player;
 
+import me.huanmeng.MengServerX;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
+import org.bukkit.plugin.Plugin;
 public class ChatFormat implements Listener {
-
+    Plugin config = MengServerX.getPlugin(MengServerX.class);
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
-        e.setFormat(
-            ChatColor.GRAY + "[" +
-            ChatColor.DARK_PURPLE + "Level: " +
-            ChatColor.GOLD +
-            player.getWorld().getName() +
-            ChatColor.GRAY + "]" +
-            ChatColor.AQUA + player.getName() +
-            ChatColor.WHITE + ": " +
-            ChatColor.RESET + e.getMessage());
+        if (config.getConfig().getBoolean("ChatFormat")) {
+            e.setFormat(
+                    ChatColor.GRAY + "[" +
+                            ChatColor.DARK_PURPLE + "Level: " +
+                            ChatColor.GOLD +
+                            player.getWorld().getName() +
+                            ChatColor.GRAY + "]" +
+                            ChatColor.AQUA + player.getName() +
+                            ChatColor.WHITE + ": " +
+                            ChatColor.RESET + e.getMessage()
+            );
+        }
     }
 }
